4b42cd4c1e6a4548d6dcddd5189f637127309dad
Ishveer-Singh <ishveersingh2007@gmail.com>
2026-06-22 22:25:20 +0530

    Edit feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,28 @@ import './App.css'
 function App() {
   const [input, setInput] = useState("")
   const [tasks, setTasks] = useState(
-     JSON.parse(localStorage.getItem("tasks")) || [])
+    JSON.parse(localStorage.getItem("tasks")) || [])
+  const [editingId, setEditingId] = useState(null)
+
+  const total = tasks.length
+  const done = tasks.filter(task => task.completed ).length
+  const notdone = tasks.filter(task => !task.completed ).length
 
   function handleadd() {
     if (input.trim() === "") return;
-    setTasks([...tasks, { id: Date.now(), task: input, completed: false }]);
+
+    if (editingId !== null) {
+      setTasks(
+        tasks.map(task =>
+          task.id === editingId
+            ? { ...task, task: input } : task
+        )
+      );
+      setEditingId(null);
+    }
+    else {
+      setTasks([...tasks, { id: Date.now(), task: input, completed: false }]);
+    }
     setInput("");
   }
 
@@ -31,7 +48,7 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
- 
+
 
   return (
     <>
@@ -46,12 +63,18 @@ function App() {
         {tasks.map((task, index) => (
           <div key={index}>
 
+
             <input type="checkbox" checked={task.completed}
               onChange={() => toggleTask(task.id)} />
 
             <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
               {task.task}
             </span>
+
+            <button onClick={() => {
+              setInput(task.task)
+              setEditingId(task.id)
+            }}>Edit</button>
 
             <button onClick={() => handledelet(task.id)}>Delet</button>
 
